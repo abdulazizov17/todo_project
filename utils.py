1acb4e5677783f0acb5f8f0196d2733a3e4425d8
Abdulazizov <asilbekabdulazizov>
2024-07-13 18:20:27 +0500

    first commit

---
+++ utils.py
@@ -0,0 +1,11 @@
+class Response:
+    def __init__(self, data: str, status_code: int):
+        self.data = data
+        self.status_code = status_code
+
+
+def hash_password(raw_password):
+    pass
+
+def match_password(raw_password,encoded_password):
+    pass
